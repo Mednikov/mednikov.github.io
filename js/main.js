ecf236ef6deb15c63ee740e6c1e4b797d4203459
Pavel <pavelnerd@gmail.com>
2019-01-21 21:32:31 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,5 +3,12 @@ $(document).ready(function(){
 	$(function () {
 	  $('[data-toggle="tooltip"]').tooltip()
 	})
+	
+	$('.scroll, .nav-link, .navbar-brand, .new-button').click(function() {
+    		var sectionTo = $(this).attr('href');
+    		$('html, body').animate({
+      			scrollTop: $(sectionTo).offset().top
+    		}, 1500);
+	});
    
 })
